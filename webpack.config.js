9a61a7aa62e11f430d1e7ed56d53d630c08f5aad
adil khan <blueaadil@gmail.com>
2020-04-25 09:03:09 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,4 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const UglifyJS = require('uglify-es');
+
 
 const DefaultUglifyJsOptions = UglifyJS.default_options();
 const compress = DefaultUglifyJsOptions.compress;
